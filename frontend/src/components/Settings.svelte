0cb183d8cd60826ce1025568c9844dbd420017c7
7Yuki <notsmile@proton.me>
2024-09-12 00:20:54 -0400

    added host and duration timeout setting

--- frontend/src/components/Settings.svelte
+++ frontend/src/components/Settings.svelte
@@ -1,12 +1,16 @@
 <script>
   import { push } from "svelte-spa-router";
+
+  const hosts = ["catbox", "pomf", "other"];
+  export let selectedHost;
+  export let timeoutDuration = 0;
 </script>
 
-<div class="div-button">
+<div>
   <button
     on:click={() => push("/")}
-    class="button-4"
-    style="background-color: rgba(0,0,0,0); border: 0px solid;  box-shadow: none; float: left"
+    class=""
+    style="background-color: rgba(0,0,0,0); border: 2px solid #FFFFFF;  box-shadow: none; float: left"
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +23,36 @@
       /></svg
     >
   </button>
+  <div>
+    <div class="center" style=" border: 2px solid gray;">
+      <label for="selected-host">Select a host</label>
+      <select name="selected-host" bind:value={selectedHost}>
+        {#each hosts as host}
+          <option value={host}>{host}</option>
+        {/each}
+      </select>
+      <span>{!selectedHost ? "Nothing selected" : selectedHost}</span>
+    </div>
+
+    <div class="center" style=" border: 2px solid gray;">
+      <label for="timeout-duration">Timeout Duration</label>
+      <input
+        name="timeout-duration"
+        type="number"
+        placeholder="5000"
+        bind:value={timeoutDuration}
+      />
+
+      <label for="timeoutDuration">Timeout Duration: {timeoutDuration}</label>
+    </div>
+  </div>
 </div>
 
 <style>
+  .center {
+    margin: auto;
+    width: 50%;
+    border: 3px solid green;
+    padding: 10px;
+  }
 </style>
