d85b3950669ecb818b400efbe9a4776db4fc5619
7Yuki <notsmile@proton.me>
2024-09-12 22:38:26 -0400

    creating preferences file

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -13,11 +16,16 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
+	fileErr := createPreferencesFile()
+	if fileErr != nil {
+		panic(fileErr)
+	}
+
 	app := NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "C2E-Wails",
+		Title:  "Clip Uploader",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
@@ -31,6 +39,27 @@ func main() {
 		Logger: nil,
 	})
 	if err != nil {
-		println("Error:", err.Error())
+		println("error: ", err.Error())
 	}
+}
+
+func createPreferencesFile() error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	prefDir := fmt.Sprintf("%s/Documents/preferences.toml", home)
+	log.Printf("pref dir: %s", prefDir)
+
+	file, err := os.Stat(prefDir)
+	if err != nil {
+		_, err := os.Create(prefDir)
+		if err != nil {
+			log.Println("what the fuck")
+			return err
+		}
+		// impl creating pref file if it doesnt exist
+	}
+	log.Printf("file exists file size: %d", file.Size())
+	return nil
 }
