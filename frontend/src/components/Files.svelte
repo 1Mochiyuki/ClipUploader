289894fb46e71cd7a3293de8794c2da06b9aa433
7Yuki <notsmile@proton.me>
2024-09-23 11:09:16 -0400

    UPDATING FILE NAME IN A NOT JANKY WAY

--- frontend/src/components/Files.svelte
+++ frontend/src/components/Files.svelte
@@ -1,151 +1,39 @@
 <script>
-  import { ChooseFile, RemovePathFromFile } from "../../wailsjs/go/main/App.js";
   import Navbar from "./Navbar.svelte";
   import { LogInfo } from "../../wailsjs/runtime/runtime.js";
-  import {
-    currentHost,
-    currentTimeoutDuration,
-    currentFiles,
-  } from "../stores.js";
-  import { Upload as UploadPomf } from "../../wailsjs/go/types/Pomf.js";
-  import { Upload as UploadLobfile } from "../../wailsjs/go/types/Lobfile.js";
-  import { Upload as UploadCatbox } from "../../wailsjs/go/types/Catbox.js";
+  import { currentHost, currentFiles as storedFiles } from "../stores.js";
+  import RemoveSection from "./fileinput/buttons/RemoveSection.svelte";
+  import UploadFile from "./fileinput/buttons/UploadFile.svelte";
+  import SelectFile from "./fileinput/buttons/SelectFile.svelte";
 
   $: newFileId = totalFiles
-    ? Math.max(...$currentFiles.map((f) => f.id)) + 1
+    ? Math.max(...currentFiles.map((f) => f.id)) + 1
     : 1;
-  $: totalFiles = $currentFiles.length;
-  $: placeholderText = "";
-  $: placeHolderEmpty = !placeholderText;
+  $: totalFiles = currentFiles.length;
   $: LogInfo(`current host: ${$currentHost}`);
-  const SELECT_A_FILE_TEXT = "Select a file";
-  /**
-   * @param {Object} file - file to upload
-   * */
-  const upload = (file) => {
-    if (!file.fileName || !$currentHost) {
-      LogInfo(`file name or current host not set`);
-      return;
-    }
-
-    LogInfo(`file name: ${file.fileName}`);
-    switch ($currentHost) {
-      case "Catbox":
-        LogInfo("catbox");
-        UploadCatbox(file.fileName);
-        break;
-      case "Lobfile":
-        LogInfo("lobfile");
-        UploadLobfile(file.fileName);
-        break;
-      case "Pomf":
-        LogInfo("pomf");
-        UploadPomf(file.fileName);
-        break;
-    }
-  };
-  /**
-   * @param {Object} file the currently selected file.
-   **/
-
-  const removeSection = (file) => {
-    LogInfo(`length: ${totalFiles} id: ${file.id}`);
-
-    //    $currentFiles = $currentFiles.filter((x) => x.id !== file.id/);
-    currentFiles.set($currentFiles.filter((x) => x.id !== file.id));
-    LogInfo(`${$currentFiles}`);
-  };
-  /**
-   *
-   * @param {Object} file the currently selected file.
-   *
-   * */
-  const getFileName = (file) => {
-    if (!file.fileName) return SELECT_A_FILE_TEXT;
-    return file.fileName;
-  };
-
-  /**
-   * @param {Object} file the currently selected file.
-   **/
-  const handleChooseFile = async (file) => {
-    placeholderText = undefined;
-
-    await ChooseFile().then((absPath) => {
-      file.absPath = absPath;
-    });
-    await RemovePathFromFile(file.absPath).then((name) => {
-      file.fileName = name;
-      placeholderText = name;
-    });
-    LogInfo(`name outside promise: ${file.fileName}`);
-  };
+  let currentFiles = [];
 
   const addFileSection = () => {
     LogInfo(`length: ${totalFiles} id: ${newFileId}`);
-    $currentFiles = [
-      ...$currentFiles,
-      { id: newFileId, fileName: "", filePath: "" },
-    ];
-    LogInfo(`${$currentFiles}`);
+    storedFiles.set([
+      ...$storedFiles,
+      { id: newFileId, fileName: "Select a file", filePath: "" },
+    ]);
+    storedFiles.update(() => (currentFiles = $storedFiles));
+    LogInfo(`${$storedFiles}`);
   };
 </script>
 
 <Navbar {addFileSection} />
 <div>
   <ul aria-labelledby="list-heading">
-    {#each $currentFiles as file}
+    {#each $storedFiles as file}
       <li>
         <div class="btn-div">
-          <button
-            on:click={() => removeSection(file)}
-            class="button-4"
-            style="padding-bottom: 3px; width: 100px;"
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="30px"
-              fill="#000000"
-              ><path
-                d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
-              /></svg
-            ></button
-          >
-          <button
-            class="button-4"
-            style="padding-bottom: 3px; width: 100px;"
-            on:click={() => upload(file)}
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="34px"
-              fill="#000000"
-              ><path
-                d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
-              /></svg
-            ></button
-          >
-
-          <button
-            class="button-4"
-            style="padding-bottom: 3px; width: 100px;"
-            on:click={() => handleChooseFile(file)}
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="34px"
-              fill="#000000"
-              ><path
-                d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
-              /></svg
-            ></button
-          >
-          <span class="fileName-text"
-            >{placeHolderEmpty ? SELECT_A_FILE_TEXT : getFileName(file)}</span
-          >
+          <RemoveSection currentFiles={$storedFiles} {file} {totalFiles} />
+          <UploadFile {file} />
+          <SelectFile id={file.id} />
+          <span class="fileName-text">{file.fileName}</span>
         </div>
       </li>
     {/each}
