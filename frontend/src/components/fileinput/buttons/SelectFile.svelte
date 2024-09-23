15b7ee431eeff7d717c8e1a3f4383855a9c3c305
7Yuki <notsmile@proton.me>
2024-09-23 11:08:55 -0400

    init

---
+++ frontend/src/components/fileinput/buttons/SelectFile.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { LogInfo } from "../../../../wailsjs/runtime/runtime.js";
+  import {
+    currentFiles as storedFiles,
+    updateFileName,
+  } from "../../../stores.js";
+  import {
+    ChooseFile,
+    RemovePathFromFile,
+  } from "../../../../wailsjs/go/main/App.js";
+
+  export let id;
+  let file = $storedFiles.find(
+    (f) => f.id === id || { fileName: "Select a file" },
+  );
+
+  /**
+   * @param {Object} file the currently selected file.
+   **/
+  const handleChooseFile = async (file) => {
+    await ChooseFile().then((absPath) => {
+      file.absPath = absPath;
+    });
+    await RemovePathFromFile(file.absPath).then((name) => {
+      LogInfo(`id from SelectFile.svelte: ${file.id}`);
+      updateFileName(id, name);
+    });
+    LogInfo(`name outside promise: ${file.fileName}`);
+  };
+</script>
+
+<button
+  class="button-4"
+  style="padding-bottom: 3px; width: 100px;"
+  on:click={() => handleChooseFile(file)}
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="34px"
+    fill="#000000"
+    ><path
+      d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
+    /></svg
+  ></button
+>
---
+++ frontend/src/components/fileinput/buttons/UploadFile.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let file;
+
+  import { Upload as UploadPomf } from "../../../../wailsjs/go/types/Pomf.js";
+  import { Upload as UploadLobfile } from "../../../../wailsjs/go/types/Lobfile.js";
+  import { Upload as UploadCatbox } from "../../../../wailsjs/go/types/Catbox.js";
+  import { currentHost, currentFiles as storedFiles } from "../../../stores.js";
+  import { LogInfo } from "../../../../wailsjs/runtime/runtime.js";
+  const upload = (file) => {
+    if (!file.fileName && $currentHost === "Select a host") {
+      LogInfo(`file name or current host not set`);
+      return;
+    }
+
+    LogInfo(`file name: ${file.fileName}`);
+    switch ($currentHost) {
+      case "Catbox":
+        LogInfo("catbox");
+        UploadCatbox(file.fileName);
+        break;
+      case "Lobfile":
+        LogInfo("lobfile");
+        UploadLobfile(file.fileName);
+        break;
+      case "Pomf":
+        LogInfo("pomf");
+        UploadPomf(file.fileName);
+        break;
+      default:
+        LogInfo("????");
+        break;
+    }
+  };
+</script>
+
+<button
+  class="button-4"
+  style="padding-bottom: 3px; width: 100px;"
+  on:click={() => upload(file)}
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="34px"
+    fill="#000000"
+    ><path
+      d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
+    /></svg
+  ></button
+>
