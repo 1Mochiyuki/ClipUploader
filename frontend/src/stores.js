606787961c0019b591928e6d580433a94c33942e
7Yuki <notsmile@proton.me>
2024-09-23 11:09:58 -0400

    added function to update file name

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -4,3 +4,6 @@ export const currentHost = writable("Select a host")
 export const currentTimeoutDuration = writable("0")
 export const currentFiles = writable([])
 
+export const updateFileName = (id, fileName) => {
+  currentFiles.update(files => files.map(file => file.id === id ? { ...file, fileName } : file))
+}
