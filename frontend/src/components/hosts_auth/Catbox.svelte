67a35b5058e6d9256bde7e0a7abd84448cd91849
1Mochiyuki <notsmile@protonmail.com>
2024-10-14 03:57:59 -0400

    init

---
+++ frontend/src/components/hosts_auth/Catbox.svelte
@@ -0,0 +1,66 @@
+<script>
+  import RemoveSection from "../fileinput/buttons/RemoveSection.svelte";
+
+  let error = "";
+  let userHash = "";
+  function handleInput() {
+    if (!userHash) {
+      error = "Please enter a string";
+      return;
+    } else if (userHash.length !== 25) {
+      error = "Not a valid Catbox User Hash";
+      return;
+    }
+    error = "";
+  }
+</script>
+
+<div class="input-container">
+  <label for="userHash">Catbox User Hash</label>
+  <input
+    id="userHash"
+    type="text"
+    bind:value={userHash}
+    placeholder="Catbox User Hash"
+    on:input={handleInput}
+  />
+  {#if error}
+    <p class="error">{error}</p>
+  {/if}
+</div>
+
+<style>
+  .input-container {
+    max-width: 22%;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 5px;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+
+  input {
+    text-align: center;
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: border-color 0.3s ease;
+  }
+
+  input:focus {
+    outline: none;
+    border-color: #4caf50;
+  }
+
+  .error {
+    color: red;
+    margin-top: 5px;
+    font-size: 14px;
+  }
+</style>
